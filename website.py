20c61b3f046ae508a31d64f7cc01a63d85da7aa5
Joachim Breitner <mail@joachim-breitner.de>
2024-04-02 22:25:17 +0200

    stash

--- website.py
+++ website.py
@@ -3,17 +3,11 @@
 import csv
 
 digest = csv.reader(open('repo-digest.csv', 'r'))
+with_nix = csv.reader(open('with-nix.log', 'r'))
 
-# parse file names in 
-import os
-import re
-
-reproducers = {}
-stage0_files = os.listdir('stage0/')
-for filename in stage0_files:
-    match = re.match(r'(.+)-from-(.+)\.sh', filename)
-    if match:
-        reproducers[match.group(1)] = match.group(2)
+naive = {}
+for (rev, _, before, tree) in with_nix:
+    naive[rev] = (before, tree)
 
 print('''
     <!doctype html>
@@ -28,6 +22,7 @@ print('''
     --font-family-mono: 'JuliaMono', monospace;
     }
     tr.good {  background-color: #e6ffe6; }
+    tr.boring {  background-color: #EEE; }
     td { white-space:nowrap; }
     td:last-child { width:100%; }
     </style>
@@ -43,33 +38,53 @@ print('''
     <tr>
     <th>status</th>
     <th>rev</th>
-    <th>stage0</th>
+    <th>expt. stage0</th>
+    <th>repr. stage0</th>
+    <th>built with</th>
     <th>comment</th>
     </tr>
     </thead>
     <tbody>
     ''')
 
-for (rev, tree, before, clean) in digest:
+for (rev, expt_tree, before, clean) in digest:
     status = "?"
-    good = False
+    clas = "unknown"
+    repr_tree = "?"
+    built_with = "?"
     comment = ""
+
     if before:
-        status = "✓"
-        good = True
-        comment = f"← <code>{before}</code> (stdlib_flags.h only)"
-    elif tree in reproducers:
-        status = "✓"
-        good = True
-        comment = f"← <code>{reproducers[tree]}</code> (nix)"
-    if clean != "true":
-        status += " <span title=\"non-stage0 chnages in commit\">⚠</a>"
+        status = "…"
+        clas = "boring"
+        repr_tree = expt_tree
+        built_with = before
+        comment = f"(stdlib_flags.h only)"
+    else:
+        if rev in naive:
+            (built_with, repr_tree) = naive[rev]
+            if repr_tree == "failed":
+                status = "☹"
+                built_with = ""
+                comment = "(rebootstrap failed)"
+            elif repr_tree == expt_tree:
+                status = "✓"
+                clas = "good"
+                comment = "(rebootstrap succeeded)"
+            else:
+                status = "😕"
+                comment = "(rebootstrap differs)"
+
+        if clean != "true":
+            status += " <span title=\"non-stage0 chnages in commit\">⚠</a>"
 
     print(f'''
-    <tr class="{"good" if good else "unknown"}">
+    <tr class="{clas}">
     <td>{status}</td>
-    <td><code>{rev}</code></td>
-    <td><code>{tree}</code></td>
+    <td><a href="https://github.com/leanprover/lean4/commit/{rev}"><code>{rev}</code></a></td>
+    <td><code>{expt_tree}</code></td>
+    <td><code>{repr_tree}</code></td>
+    <td><code>{built_with}</code></td>
     <td>{comment}</td>
     </tr>
     ''')
